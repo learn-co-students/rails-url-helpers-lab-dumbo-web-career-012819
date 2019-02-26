7bce2ef3f859d5a43bd71085b248db6e210dffc3
mazenswar <mazenswar@protonmail.com>
2019-02-26 08:14:23 -0500

    url helpers

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,31 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+    # byebug
+    if @student.active
+      @status = "This student is currently active."
+    else
+      @status = "This student is currently inactive."
+    end
+  end
+
+  def update
+    set_student
+    # @student.update(active: !@student.active)
+    # # byebug
+    # redirect_to student_path(@student)
+  end
+
+  def activate
+    set_student
+    @student.update(active: !@student.active)
+    redirect_to student_path(@student)
   end
 
   private
